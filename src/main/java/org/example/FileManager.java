c9c8aa45745afa9131dede130bf1cd6aa5ee03fa
Juan Volpintesta <juanvolpintesta@uca.edu.ar>
2024-09-02 20:31:15 -0300

    implementando file manager

--- src/main/java/org/example/FileManager.java
+++ src/main/java/org/example/FileManager.java
@@ -1,4 +1,40 @@
 package org.example;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class FileManager {
+    List<String> lineasDelArchivo = new ArrayList<>();
+    List<Pregunta> preguntas = new ArrayList<>();
+
+    public List<String> leerArchivo(String pathToFile) {
+        try {
+            File archivoConLineas = new File(pathToFile);
+            Scanner lectorDeArchivo = new Scanner(archivoConLineas);
+            while (lectorDeArchivo.hasNextLine()) {
+                String data = lectorDeArchivo.nextLine();
+                lineasDelArchivo.add(data);
+            }
+            lectorDeArchivo.close();
+        } catch (
+                FileNotFoundException e) {
+            System.out.println("Archivo no encontrado.");
+            e.printStackTrace();
+        }
+        return lineasDelArchivo;
+    }
+
+    public List<Pregunta> leerPreguntas(List<String> lineasDelArchivo) {
+        for (String linea : lineasDelArchivo) {
+            List<String> cadaLinea = new ArrayList<>(Arrays.stream(linea.split(",")).toList());
+            List<String> cadaRespuesta = new ArrayList<>(Arrays
+                    .stream(cadaLinea.get(1).split("&")).toList());
+            preguntas.add(new Pregunta(cadaLinea.get(0), cadaRespuesta, Integer.parseInt(cadaLinea.get(2))));
+        }
+        return preguntas;
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,26 +7,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Quiz quiz = new Quiz();
-        List<String> lineasDelArchivo = new ArrayList<>();
-        List<Pregunta> preguntas = new ArrayList<>();
-        try {
-            File archivoConLineas = new File("src/main/resources/preguntas");
-            Scanner lectorDeArchivo = new Scanner(archivoConLineas);
-            while (lectorDeArchivo.hasNextLine()) {
-                String data = lectorDeArchivo.nextLine();
-                lineasDelArchivo.add(data);
-            }
-            lectorDeArchivo.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("Archivo no encontrado.");
-            e.printStackTrace();
-        }
-        for (String linea : lineasDelArchivo) {
-            List<String> cadaLinea = new ArrayList<>(Arrays.stream(linea.split(",")).toList());
-            List<String> cadaRespuesta = new ArrayList<>(Arrays
-                    .stream(cadaLinea.get(1).split("&")).toList());
-            preguntas.add(new Pregunta(cadaLinea.get(0), cadaRespuesta, Integer.parseInt(cadaLinea.get(2))));
-        }
-        quiz.iniciarQuiz(preguntas);
+        FileManager fileManager = new FileManager();
+        quiz.iniciarQuiz(fileManager.leerPreguntas(fileManager.leerArchivo("src/main/resources/preguntas")));
     }
 }
